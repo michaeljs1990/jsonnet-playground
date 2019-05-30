8c6d9f34a8416a5506a17eb5a01f942d063a7686
Michael Schuett <michaeljs1990@gmail.com>
2019-05-30 00:49:53 -0400

    Setup basic fileserver for static assets
    Going to go with a web server that handles serving up frontend assets
    and a /backend path for all the needed compile/fmt/whatever stuff

--- edit.go
+++ edit.go
@@ -1,1 +1,10 @@
 package main
+
+import (
+  "net/http"
+  "fmt"
+)
+
+func (s *Server) HandleEdit(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "ok")
+}
--- main.go
+++ main.go
@@ -10,6 +10,13 @@ type Server struct {
   mux *http.ServeMux
 }
 
+func (s *Server) Init() {
+  s.mux.HandleFunc("/backend", s.HandleEdit)
+
+  // Catch all for serving up static assets
+  s.mux.Handle("/", http.FileServer(http.Dir("./static")))
+}
+
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	s.mux.ServeHTTP(w, r)
 }
@@ -18,6 +25,8 @@ func main() {
   server := &Server{
     mux: http.NewServeMux(),
   }
+
+  server.Init()
 
   port := os.Getenv("PORT")
 	if port == "" {
