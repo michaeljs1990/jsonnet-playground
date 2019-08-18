9bf2f8939e2bb66d8c59c9a16e155e6a03f446c1
Michael Schuett <michaeljs1990@gmail.com>
2019-08-17 21:38:56 -0400

    Basic functionality
    This is a bare bones editor for jsonnet using the golang VM

--- exec.go
+++ exec.go
@@ -1,19 +1,40 @@
 package main
 
 import (
-  "github.com/google/go-jsonnet"
+	"fmt"
+	"net/http"
+
+	"github.com/google/go-jsonnet"
 )
 
-func execJsonnetCode(code string) (string, error) {
-  vm := jsonnet.MakeVM()
-  // The first param is a filename for when an error is output. As we are doing this all in memory
-  // having a filename returned for an error would likely just be confusing to a user.
-  out, err := vm.EvaluateSnippet("", code)
-  if err != nil {
-    // Explicitly return an emptry string otherwise it will just return x as out which
-    // may be confusing to people when it shows up in the editor.
-    return "", err
-  }
+func (s *Server) HandleCompile(w http.ResponseWriter, r *http.Request) {
 
-  return out, nil
+	// Use MB of memory before paging out to disk
+	if err := r.ParseMultipartForm(1000000); err != nil {
+		fmt.Fprintf(w, "Unable to parse form: %v", err)
+		return
+	}
+
+	// fmt.Fprintf(w, "Post from website! %s", r.Form["jsonnet"])
+	ret, err := execJsonnetCode(r.FormValue("jsonnet"))
+	if err != nil {
+		fmt.Fprintf(w, "Unable to exec code with jsonnet VM: %v", err)
+		return
+	}
+
+	fmt.Fprintf(w, ret)
+}
+
+func execJsonnetCode(code string) (string, error) {
+	vm := jsonnet.MakeVM()
+	// The first param is a filename for when an error is output. As we are doing this all in memory
+	// having a filename returned for an error would likely just be confusing to a user.
+	out, err := vm.EvaluateSnippet("", code)
+	if err != nil {
+		// Explicitly return an emptry string otherwise it will just return x as out which
+		// may be confusing to people when it shows up in the editor.
+		return "", err
+	}
+
+	return out, nil
 }
--- main.go
+++ main.go
@@ -1,37 +1,37 @@
 package main
 
 import (
-  "net/http"
-  "log"
-  "os"
+	"log"
+	"net/http"
+	"os"
 )
 
 type Server struct {
-  mux *http.ServeMux
-}
-
-func (s *Server) Register() {
-  s.mux.HandleFunc("/backend", s.HandleEdit)
-
-  // Catch all for serving up static assets
-  s.mux.Handle("/", http.FileServer(http.Dir("./static")))
+	mux *http.ServeMux
 }
 
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	s.mux.ServeHTTP(w, r)
 }
 
+func (s *Server) Register() {
+	s.mux.HandleFunc("/backend/compile", s.HandleCompile)
+
+	// Catch all for serving up static assets
+	s.mux.Handle("/", http.FileServer(http.Dir("./static")))
+}
+
 func main() {
-  server := &Server{
-    mux: http.NewServeMux(),
-  }
+	server := &Server{
+		mux: http.NewServeMux(),
+	}
 
-  server.Register()
+	server.Register()
 
-  port := os.Getenv("PORT")
+	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
-  }
+	}
 
-  log.Fatalf("Error listening on :%v: %v", port, http.ListenAndServe(":"+port, server))
+	log.Fatalf("Error listening on :%v: %v", port, http.ListenAndServe(":"+port, server))
 }
